dc7f5fe32ed794a5be850fa20397e0a6cfe545c7
Ben <benthedev@outlook.com>
2018-12-20 16:46:46 +0000

    removed test file and completed btree.pl

--- BinaryTreeAssignment/bTree.pl
+++ BinaryTreeAssignment/bTree.pl
@@ -27,11 +27,11 @@ insert(X,bTree(Root,Left,Right),bTree(Root,Left,NewRight)) :-
    insert(X,Right,NewRight).
 
 %Preorder Traversal -> Left Tree First then right tree
+preorder(nil,[]).
 preorder(bTree(X,Left,Right),[X|Nodes]) :-
    preorder(Left,LeftNodes),
    preorder(Right,RightNodes),
    append(LeftNodes,RightNodes,Nodes).
-preorder(nil,[]).
 
 %Inorder Traversal ->
 inorder(nil,[]).
@@ -40,9 +40,18 @@ inorder(bTree(X,Left,Right),Nodes) :-
    inorder(Right,RightNodes),
    append(LeftNodes,[X|RightNodes],Nodes).
 
+%Postorder Traversal
+postorder(nil,[]).
+postorder(bTree(X,Left,Right),Nodes) :-
+   postorder(Left,LeftNodes),
+   postorder(Right,RightNodes),
+   append(LeftNodes,RightNodes,CombinedNodes),
+   append(CombinedNodes,[X],Nodes).
+
 %Height of Binary tree
+height(nil,0).
 height(bTree(_,nil,nil),1).
 height(bTree(_,Left,Right),TotalHeight) :-
     height(Left,TotalLeft),
     height(Right,TotalRight),
-    TotalHeight is max(TotalLeft,TotalRight) + 1.
+    TotalHeight is max(TotalLeft,TotalRight) + 1,!.
